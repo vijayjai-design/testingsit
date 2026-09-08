f7467f5ba5783fb56e74837d9685a49300d26a2f
vijayjai-design <vijayjai@iastate.edu>
2026-09-08 12:50:28 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,22 +1,10 @@
-// navigation.js
+const navbar = document.getElementById("navbar");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const nav = document.createElement("nav");
-
-  nav.className = "main-nav";
-
-  nav.innerHTML = `
-    <div class="nav-container">
-      <a class="nav-brand" href="index.html">Vijay Jairam</a>
-
-      <div class="nav-links">
-        <a href="index.html">About Me</a>
-        <a href="personal.html">Personal</a>
-        <a href="education.html">Education</a>
-        <a href="experience.html">Experience</a>
-      </div>
-    </div>
-  `;
-
-  document.body.prepend(nav);
-});
+navbar.innerHTML = `
+  <nav class="nav-container">
+    <a href="index.html">About Me</a>
+    <a href="personal.html">Personal</a>
+    <a href="education.html">Education</a>
+    <a href="experience.html">Experience</a>
+  </nav>
+`;
